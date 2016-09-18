979456bf99cacefaa75061bb51a453cb34e88c17
Giorgi Kotchlamazashvili <georgedot@gmail.com>
2016-09-18 21:07:36 +0400

    playCounts and statistics + bigger progress bar

--- www/index.php
+++ www/index.php
@@ -21,6 +21,14 @@ foreach ($xpCapPerLevel as $levelIndex => $levelCap) {
 
 $levelProgressPercent = round(($currentPartyXP / $nextLevelXP ) * 100, 2);
 
+$sessionsPlayed = $data['playCounts']['session'];
+$oneShotsPlayed = $data['playCounts']['oneShot'];
+$totalPlays = $sessionsPlayed + $oneShotsPlayed;
+
+$xpNeededToLevelUp = $nextLevelXP - $currentPartyXP;
+$avgXpPerSession = $currentPartyXP / $totalPlays;
+$approxSessionsForLevelUp = $xpNeededToLevelUp / $avgXpPerSession;
+
 echo '<!DOCTYPE html>'
   . '<html lang="en">'
   . '<head>'
@@ -44,28 +52,44 @@ echo '<!DOCTYPE html>'
           . '<tbody>'
             . '<tr>'
               . '<th>Location</th>'
-              . '<td><span class="fa fa-location-arrow"></span> ' . htmlspecialchars($currentPartyLocation) . '</td>'
+              . '<td><span class="fa fa-location-arrow"></span> '
+                . htmlspecialchars($currentPartyLocation)
+              . '</td>'
             . '</tr>'
             . '<tr>'
               . '<th>Party XP</th>'
               . '<td>'
-                . '<div class="progress" style="position:relative">'
-                  . '<div class="progress-bar" role="progressbar" '
+                . '<div class="progress" style="position:relative; height: 40px;">'
+                  . '<div class="progress-bar progress-bar-success" role="progressbar" '
                   . 'aria-valuenow="'.ceil($levelProgressPercent).'" aria-valuemin="0" aria-valuemax="100"'
                   . 'style="width:'.ceil($levelProgressPercent).'%">'
-                    . '<div style="position:absolute;z-index: 2;left: 0;right: 0; color:#ccc;">'
+                    . '<div style="position:absolute;z-index: 2; top:0; bottom: 0; left: 0; right:0;'
+                    . 'line-height:40px; color:#333;font-weight: bold; font-size: 1.5em;">'
                       . htmlspecialchars(number_format($currentPartyXP) . ' (lv. '. $currentPartyLevelName . ')')
                       . ' / '
                       . htmlspecialchars(number_format($nextLevelXP) . ' (lv. '. $nextLevelName . ')')
-                    .'</div>'
-                    .'<span class="sr-only">'. $levelProgressPercent.'% Complete</span>'
-                  .'</div>'
-                .'</div>'
+                    . '</div>'
+                    . '<span class="sr-only">'. $levelProgressPercent.'% Complete</span>'
+                  . '</div>'
+                . '</div>'
+                . '<p class="text-muted">'
+                  . 'You need to play need Approx. '
+                  . floor($approxSessionsForLevelUp) .'-'. ceil($approxSessionsForLevelUp)
+                  . ' more session to level up'
+                . '</p>'
               . '</td>'
             . '</tr>'
             . '<tr>'
               . '<th>Sessions Played</th>'
-              . '<td><span class="fa fa-calculator"></span> 7 + 2 = 9</td>'
+              . '<td><span class="fa fa-calculator"></span> '
+                . htmlspecialchars(
+                  $sessionsPlayed
+                  . ' + '
+                  . $oneShotsPlayed
+                  . ' = ' . $totalPlays . ' '
+                  . ' (approx '. round($avgXpPerSession, 2) .' xp per session)'
+                )
+              . '</td>'
             . '</tr>'
           . '</tbody>'
         . '</table>'
