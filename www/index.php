7f27c9342d6fbf0f6a24642de9c7460a0943978a
Giorgi Kotchlamazashvili <georgedot@gmail.com>
2016-09-18 20:17:45 +0400

    styles and progress bar

--- www/index.php
+++ www/index.php
@@ -1,48 +1,77 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-  <meta name="description" content="">
-  <meta name="author" content="">
+<?php
 
-  <title>Starter Template for Bootstrap</title>
+$data = json_decode(file_get_contents('./index.json'), true);
 
-  <!-- Bootstrap core CSS -->
-  <link href="/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet">
-</head>
+$currentPartyLocation = $data['current']['location'];
+$currentPartyXP = $data['current']['partyXp'];
 
-<body>
+$xpCapPerLevel = $data['xpCapPerLevel'];
+$currentPartyLevelName = "1";
+$nextLevelName = "?";
+$nextLevelXP = null;
 
-<div class="container" style="margin-top: 20px;">
-  <div class="header clearfix">
-    <h3 class="text-muted">Cult of the Treacherous God</h3>
-  </div>
+foreach ($xpCapPerLevel as $levelIndex => $levelCap) {
+  if ($levelCap >= $currentPartyXP) {
+    $nextLevelName = (string)($levelIndex + 2);
+    $nextLevelXP = $levelCap;
+    break;
+  }
+  $currentPartyLevelName = (string)($levelIndex + 2);
+}
 
-  <div class="jumbotron">
-    <h1><span class="fa fa-feed"></span> Current Status</h1>
-    <table class="table lead">
-      <tbody>
-      <tr>
-        <th>Location</th>
-        <td><span class="fa fa-location-arrow"></span> "Wasted Captain" Guild</td>
-      </tr>
-      <tr>
-        <th>Party XP</th>
-        <td><span class="fa fa-beer"></span> 3570 (lv 4) / 6500 (lv 5)</td>
-      </tr>
-      <tr>
-        <th>Sessions Played</th>
-        <td><span class="fa fa-calculator"></span> 7 + 2 = 9</td>
-      </tr>
-      </tbody>
-    </table>
-  </div>
-</div>
-<script src="/vendor/jquery/jquery.min.js"></script>
-<script src="/vendor/bootstrap/js/bootstrap.min.js"></script>
-</body>
-</html>
+$levelProgressPercent = round(($currentPartyXP / $nextLevelXP ) * 100, 2);
+
+echo '<!DOCTYPE html>'
+  . '<html lang="en">'
+  . '<head>'
+    . '<meta charset="utf-8">'
+    . '<meta http-equiv="X-UA-Compatible" content="IE=edge">'
+    . '<meta name="viewport" content="width=device-width, initial-scale=1">'
+    . '<meta name="description" content="">'
+    . '<meta name="author" content="">'
+    . '<title>Cult of the Treacherous God</title>'
+    . '<link href="/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">'
+    . '<link href="/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet">'
+  . '</head>'
+  . '<body>'
+    . '<div class="container" style="margin-top: 20px;">'
+      . '<div class="header clearfix">'
+        . '<h3 class="text-muted">Cult of the Treacherous God</h3>'
+      . '</div>'
+      . '<div class="jumbotron">'
+        . '<h1><span class="fa fa-feed"></span> Current Status</h1>'
+        . '<table class="table lead">'
+          . '<tbody>'
+            . '<tr>'
+              . '<th>Location</th>'
+              . '<td><span class="fa fa-location-arrow"></span> ' . htmlspecialchars($currentPartyLocation) . '</td>'
+            . '</tr>'
+            . '<tr>'
+              . '<th>Party XP</th>'
+              . '<td>'
+                . '<div class="progress" style="position:relative">'
+                  . '<div class="progress-bar" role="progressbar" '
+                  . 'aria-valuenow="'.ceil($levelProgressPercent).'" aria-valuemin="0" aria-valuemax="100"'
+                  . 'style="width:'.ceil($levelProgressPercent).'%">'
+                    . '<div style="position:absolute;z-index: 2;left: 0;right: 0; color:#ccc;">'
+                      . htmlspecialchars(number_format($currentPartyXP) . ' (lv. '. $currentPartyLevelName . ')')
+                      . ' / '
+                      . htmlspecialchars(number_format($nextLevelXP) . ' (lv. '. $nextLevelName . ')')
+                    .'</div>'
+                    .'<span class="sr-only">'. $levelProgressPercent.'% Complete</span>'
+                  .'</div>'
+                .'</div>'
+              . '</td>'
+            . '</tr>'
+            . '<tr>'
+              . '<th>Sessions Played</th>'
+              . '<td><span class="fa fa-calculator"></span> 7 + 2 = 9</td>'
+            . '</tr>'
+          . '</tbody>'
+        . '</table>'
+      . '</div>'
+    . '</div>'
+    . '<script src="/vendor/jquery/jquery.min.js"></script>'
+    . '<script src="/vendor/bootstrap/js/bootstrap.min.js"></script>'
+  . '</body>'
+  . '</html>';
